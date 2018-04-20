94ef17b601a74f40ec52fcd809a7285a761b568e
Vignesh Ramachandra <vickyramachandra@gmail.com>
2018-04-20 10:55:37 +0530

    Nested relationship annotations changes

--- jsonsideload.go
+++ jsonsideload.go
@@ -20,17 +20,21 @@ func Unmarshal(jsonPayload []byte, model interface{}) error {
 
 const (
 	annotationJSONSideload    = "jsonsideload"
-	annotationAttribute       = "attr"
+	annotationInclude         = "include"
+	annotationIncludes        = "includes"
 	annotationHasOneRelation  = "hasone"
 	annotationHasManyRelation = "hasmany"
 )
 
 func unMarshalNode(sourceMap, mapToParse map[string]interface{}, model reflect.Value) (err error) {
+	// recovering for any wrong representation in struct
 	defer func() {
 		if r := recover(); r != nil {
 			err = fmt.Errorf("Data is not a jsonsideload representation of '%v'", model.Type())
 		}
 	}()
+
+	// First, doing a json unmarshal to make sure all primitive types are mapped correct
 	jsonString, err := json.Marshal(mapToParse)
 	if err != nil {
 		return err
@@ -43,41 +47,36 @@ func unMarshalNode(sourceMap, mapToParse map[string]interface{}, model reflect.V
 	modelType := model.Type().Elem()
 
 	var er error
+	// Now going through all the fields of the struct
 	for i := 0; i < modelValue.NumField(); i++ {
 		fieldType := modelType.Field(i)
 		tag := fieldType.Tag.Get(annotationJSONSideload)
-		if tag == "" {
+		if tag == "" { // Ignoring the fields which doesn't have 'jsonsideload' tags
 			continue
 		}
 
 		fieldValue := modelValue.Field(i)
 		args := strings.Split(tag, ",")
-		if len(args) < 1 {
+		if len(args) < 1 { // Error, if there aren't any realationship with the tag
 			er = errors.New("Bad jsonsideload struct tag format")
 			break
 		}
 		annotation := args[0]
-		relation := args[1]
 
-		if annotation == annotationHasOneRelation {
-			if fieldValue.Kind() != reflect.Ptr {
+		// annotation includes means the object is already nested and not sideloaded
+		if annotation == annotationInclude {
+			if fieldValue.Kind() != reflect.Ptr { // Only pointer types are allowed in struct
 				return fmt.Errorf("Expecting pointer type for %s in struct", fieldType.Name)
 			}
+			if len(args) < 2 {
+				return fmt.Errorf("No relationship found in annotation for %s", fieldType.Name)
+			}
+			relation := args[1]
 			var relationMap map[string]interface{}
-			if len(args) < 3 { // this means the json is already nested
-				relationObj := mapToParse[relation]
-				if relationObj != nil {
-					if mapObj, ok := relationObj.(map[string]interface{}); ok {
-						relationMap = mapObj
-					}
-				}
-			} else {
-				relationID := mapToParse[args[2]]
-				if relationID != nil {
-					valueMap := getValueFromSourceJSON(sourceMap, relation, relationID.(float64))
-					if valueMap != nil {
-						relationMap = valueMap.(map[string]interface{})
-					}
+			relationObj := mapToParse[relation]
+			if relationObj != nil {
+				if mapObj, ok := relationObj.(map[string]interface{}); ok {
+					relationMap = mapObj
 				}
 			}
 			m := reflect.New(fieldValue.Type().Elem())
@@ -88,15 +87,70 @@ func unMarshalNode(sourceMap, mapToParse map[string]interface{}, model reflect.V
 				}
 			}
 			fieldValue.Set(m)
-		} else if annotation == annotationHasManyRelation {
-			if len(args) < 3 { // this means the array is already nested
-				models := reflect.New(fieldValue.Type()).Elem()
-				hasManyRelations := mapToParse[args[1]]
-				if hasManyRelations != nil {
-					if relationsArray, ok := hasManyRelations.([]interface{}); ok {
-						for _, n := range relationsArray {
-							m := reflect.New(fieldValue.Type().Elem().Elem())
-							if err := unMarshalNode(sourceMap, n.(map[string]interface{}), m); err != nil {
+		} else if annotation == annotationIncludes { // annotation includes mean, the array is already nested and not sideloaded
+			if len(args) < 2 {
+				return fmt.Errorf("No relationship found in annotation for %s", fieldType.Name)
+			}
+			if fieldValue.Type().Elem().Kind() != reflect.Ptr {
+				return fmt.Errorf("Expecting array of pointers for %s in struct", fieldType.Name)
+			}
+			relation := args[1]
+			models := reflect.New(fieldValue.Type()).Elem()
+			hasManyRelations := mapToParse[relation]
+			if hasManyRelations != nil {
+				if relationsArray, ok := hasManyRelations.([]interface{}); ok {
+					for _, n := range relationsArray {
+						m := reflect.New(fieldValue.Type().Elem().Elem())
+						if err := unMarshalNode(sourceMap, n.(map[string]interface{}), m); err != nil {
+							er = err
+							break
+						}
+						models = reflect.Append(models, m)
+					}
+				}
+			}
+			fieldValue.Set(models)
+		} else if annotation == annotationHasOneRelation { // hasone means, the relationship is sideloaded
+			if fieldValue.Kind() != reflect.Ptr {
+				return fmt.Errorf("Expecting pointer type for %s in struct", fieldType.Name)
+			}
+			if len(args) < 2 {
+				return fmt.Errorf("No relationship found in annotation for %s", fieldType.Name)
+			}
+			var relationMap map[string]interface{}
+			relation := args[1]
+			relationID := mapToParse[args[2]]
+			if relationID != nil { // using the relationID, search the source tree for the relationship
+				valueMap := getValueFromSourceJSON(sourceMap, relation, relationID.(float64))
+				if valueMap != nil {
+					relationMap = valueMap.(map[string]interface{})
+				}
+			}
+			m := reflect.New(fieldValue.Type().Elem())
+			if relationMap != nil {
+				if err := unMarshalNode(sourceMap, relationMap, m); err != nil {
+					er = err
+					break
+				}
+			}
+			fieldValue.Set(m)
+		} else if annotation == annotationHasManyRelation { // hasmany means, the relationships is sideloaded
+			if len(args) < 2 {
+				return fmt.Errorf("No relationship found in annotation for %s", fieldType.Name)
+			}
+			if fieldValue.Type().Elem().Kind() != reflect.Ptr {
+				return fmt.Errorf("Expecting array of pointers for %s in struct", fieldType.Name)
+			}
+			models := reflect.New(fieldValue.Type()).Elem()
+			relation := args[1]
+			hasManyRelations := mapToParse[args[2]]
+			if hasManyRelations != nil {
+				if relationsArray, ok := hasManyRelations.([]interface{}); ok {
+					for _, n := range relationsArray { // range on the array of relationship IDS and get each relationship from the source tree
+						m := reflect.New(fieldValue.Type().Elem().Elem())
+						relationMap := getValueFromSourceJSON(sourceMap, relation, n.(float64))
+						if relationMap != nil {
+							if err := unMarshalNode(sourceMap, relationMap.(map[string]interface{}), m); err != nil {
 								er = err
 								break
 							}
@@ -104,27 +158,8 @@ func unMarshalNode(sourceMap, mapToParse map[string]interface{}, model reflect.V
 						}
 					}
 				}
-				fieldValue.Set(models)
-			} else {
-				models := reflect.New(fieldValue.Type()).Elem()
-				hasManyRelations := mapToParse[args[2]]
-				if hasManyRelations != nil {
-					if relationsArray, ok := hasManyRelations.([]interface{}); ok {
-						for _, n := range relationsArray {
-							m := reflect.New(fieldValue.Type().Elem().Elem())
-							relationMap := getValueFromSourceJSON(sourceMap, relation, n.(float64))
-							if relationMap != nil {
-								if err := unMarshalNode(sourceMap, relationMap.(map[string]interface{}), m); err != nil {
-									er = err
-									break
-								}
-								models = reflect.Append(models, m)
-							}
-						}
-					}
-				}
-				fieldValue.Set(models)
 			}
+			fieldValue.Set(models)
 		}
 	}
 	return er
