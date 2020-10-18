68b42c40fbc7727a96b8074769513f2b73f5e24d
Vignesh Ramachandra <vickyramachandra@gmail.com>
2020-10-18 07:09:17 +0530

    Update models.go

--- models.go
+++ models.go
@@ -7,10 +7,12 @@ import (
 	mytime "github.com/vickyramachandra/time"
 )
 
+// PersonResponse is the response struct
 type PersonResponse struct {
 	Persons []*Person `json:"persons" jsonsideload:"includes,persons"`
 }
 
+// Person is as the name says
 type Person struct {
 	ID          json.Number   `json:"id"`
 	Name        string        `json:"name"`
@@ -20,6 +22,7 @@ type Person struct {
 	ShortDob    *mytime.Ctime `json:"short_dob"`
 }
 
+// City is as the name says
 type City struct {
 	ID   float64 `json:"id"`
 	Name string  `json:"name"`
